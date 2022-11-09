d9a56b532284d2bfba9c1eecbec06d2fa689498d
Burak Çakır <burak2043@gmail.com>
2022-11-09 16:55:48 +0300

    project started

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const notRoute = require("./routes/notlar")
+
+
+//loglama
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+app.use(express.json());
+
+app.use("/", notRoute);
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log("server is running on port : http://localhost:" + PORT);
+})
